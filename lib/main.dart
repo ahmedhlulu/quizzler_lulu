867683b5f0663a626ff5b7563d432882b463d31e
ahmedhlulu <ahmedhlulu@icloud.com>
2023-06-14 03:01:45 +0300

    add alert

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,140 @@
 import 'package:flutter/material.dart';
 import 'package:quizzler_lulu/quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: const QuizPage(),
+      ),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class QuizPage extends StatefulWidget {
+  const QuizPage({Key? key}) : super(key: key);
 
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
   List<Icon> iconList = [];
-  Icon iconTrue = const Icon(Icons.check,color: Colors.green);
-  Icon iconFalse = const Icon(Icons.close,color: Colors.red);
+  List<Icon> iconList2 = [];
+
+  Icon iconTrue = const Icon(Icons.check, color: Colors.green);
+  Icon iconFalse = const Icon(Icons.close, color: Colors.red);
 
   QuizBrain quizBrain = QuizBrain();
 
-  void checkAnswer(bool userChoice){
+  int score = 0;
+  int scoreFinish = 0;
+
+  void checkAnswerChoice(bool userChoice) {
     setState(() {
-      if(quizBrain.getQuestionAnswer() == userChoice) {
+      if (quizBrain.getQuestionAnswer() == userChoice) {
         iconList.add(iconTrue);
-      }else {
+        score += 1;
+      } else {
         iconList.add(iconFalse);
       }
       quizBrain.nextQuestion();
     });
   }
 
+  void reset(){
+    setState(() {
+      iconList2 = iconList;
+      iconList = [];
+      scoreFinish = score;
+      score = 0;
+      quizBrain.reset();
+    });
+  }
+
+  void setAlert(){
+    Alert(
+      context: context,
+      title: "You are done!",
+      desc: "Your Score is: $scoreFinish / ${quizBrain.questionsLength()}",
+      content: Wrap(children: iconList2),
+      type: AlertType.success,
+      onWillPopActive: true,
+      style: AlertStyle(
+        isCloseButton: false,
+        backgroundColor: Colors.grey.shade800,
+        titleStyle: TextStyle(color: Colors.white),
+        descStyle: TextStyle(color: Colors.white),
+      ),
+    ).show();
+  }
+
+  void checkAnswer(bool userChoice) {
+    if (quizBrain.isFinished()) {
+      checkAnswerChoice(userChoice);
+      reset();
+      setAlert();
+    } else {
+      checkAnswerChoice(userChoice);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.grey.shade800,
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              flex: 5,
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Center(
-                  child: Text(
-                    quizBrain.getQuestionText(),
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                        fontSize: 25,
-                        color: Colors.white
-                    ),
-                  ),
-                ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestionText(),
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 25, color: Colors.white),
               ),
             ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: ElevatedButton(
-                  onPressed: () => checkAnswer(true),
-                  style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.green)),
-                  child: const Text(
-                    "True",
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: ElevatedButton(
-                  onPressed: () => checkAnswer(false),
-                  style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.red)),
-                  child: const Text(
-                    "False",
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                ),
-              ),
-            ),
-            Row(children: iconList),
-          ],
+          ),
         ),
-      ),
+        Wrap(children: iconList, alignment: WrapAlignment.center),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: ElevatedButton(
+              onPressed: () => checkAnswer(true),
+              style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.green)),
+              child: const Text(
+                "True",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: ElevatedButton(
+              onPressed: () => checkAnswer(false),
+              style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.red)),
+              child: const Text(
+                "False",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -1,32 +1,45 @@
 import 'package:quizzler_lulu/question.dart';
 
 class QuizBrain {
-  int questionNo = 0;
+  int _questionNo = 0;
 
   final List<Question> _questionBank = [
+    Question('Some cats are actually allergic to humans', true),
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true),
+    Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
+    Question('It is illegal to pee in the Ocean in Portugal.', true),
+    Question('No piece of square dry paper can be folded in half more than 7 times.', false),
     Question(
-      questionText: 'You can Lead a cow down stairs but not up stairs.',
-      questionAnswer: false,
-    ),
-    Question(
-      questionText: 'Approximately one quarter of human bones are in the feet.',
-      questionAnswer: true,
-    ),
-    Question(
-      questionText: 'A slug\'s blood is green.',
-      questionAnswer: true,
-    ),
+        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        true),
+    Question('The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.', false),
+    Question('The total surface area of two human lungs is approximately 70 square metres.', true),
+    Question('Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.', true),
+    Question('In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.', true),
   ];
 
-  String getQuestionText() => _questionBank[questionNo].questionText;
+  String getQuestionText() => _questionBank[_questionNo].questionText;
 
-  bool getQuestionAnswer() => _questionBank[questionNo].questionAnswer;
+  bool getQuestionAnswer() => _questionBank[_questionNo].questionAnswer;
 
   void nextQuestion() {
-    if(questionNo < _questionBank.length -1){
-      questionNo++;
+    if (_questionNo < _questionBank.length - 1) {
+      _questionNo++;
     }
   }
 
-}
+  bool isFinished(){
+    if(_questionNo == _questionBank.length - 1){
+      return true;
+    }else{
+      return false;
+    }
+  }
 
+  void reset() => _questionNo = 0;
+
+  int questionsLength() => _questionBank.length;
+
+}
